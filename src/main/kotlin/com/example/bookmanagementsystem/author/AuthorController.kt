74faeaf549536319dc512829b9cfb7bac3b5bb17
1hakusai1 <1hakusai1@gmail.com>
2024-09-24 14:33:34 +0900

    authorのgetとlistができるように

--- src/main/kotlin/com/example/bookmanagementsystem/author/AuthorController.kt
+++ src/main/kotlin/com/example/bookmanagementsystem/author/AuthorController.kt
@@ -1,13 +1,25 @@
 package com.example.bookmanagementsystem.author
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/author")
 class AuthorController(val repository: AuthorRepository) {
     @GetMapping("")
-    fun listAuthors(): Map<String, String> {
-        return mapOf("name" to "dummy")
+    fun listAuthors(): List<Author> {
+        return repository.list()
+    }
+
+    @GetMapping("/{id}")
+    fun findAuthor(@PathVariable("id") id: String): ResponseEntity<Author?> {
+        val found = repository.findById(id)
+        return if (found == null) {
+            ResponseEntity(null, HttpStatus.NOT_FOUND)
+        } else {
+            ResponseEntity(found, HttpStatus.OK)
+        }
     }
 
     @PostMapping("")
--- src/main/kotlin/com/example/bookmanagementsystem/author/AuthorRepository.kt
+++ src/main/kotlin/com/example/bookmanagementsystem/author/AuthorRepository.kt
@@ -2,11 +2,34 @@ package com.example.bookmanagementsystem.author
 
 import com.example.bookmanagementsystem.jooq.public_.tables.Authors.AUTHORS
 import org.jooq.DSLContext
+import org.jooq.Record2
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 class AuthorRepository(val dslContext: DSLContext) {
+
+    fun list(): List<Author> {
+        val response = dslContext
+            .select(AUTHORS.ID, AUTHORS.NAME)
+            .from(AUTHORS)
+            .fetch()
+        return response.map { toAuthor(it) }
+    }
+
+    fun findById(id: String): Author? {
+        val response = dslContext
+            .select(AUTHORS.ID, AUTHORS.NAME)
+            .from(AUTHORS)
+            .where(AUTHORS.ID.eq(UUID.fromString(id)))
+            .fetchOne()
+        return response?.let { toAuthor(it) }
+    }
+
+    private fun toAuthor(response: Record2<UUID, String>) = Author(
+        response.getValue(AUTHORS.ID).toString(),
+        response.getValue(AUTHORS.NAME)
+    )
 
     fun save(author: Author) {
         dslContext.newRecord(AUTHORS).also {
