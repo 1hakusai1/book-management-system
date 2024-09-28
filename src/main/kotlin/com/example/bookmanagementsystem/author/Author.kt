25cce6d24bacf4d4400b89da87469b15cb855082
1hakusai1 <1hakusai1@gmail.com>
2024-09-28 20:18:27 +0900

    生年月日は過去の日付のみ登録できるように

--- src/main/kotlin/com/example/bookmanagementsystem/author/Author.kt
+++ src/main/kotlin/com/example/bookmanagementsystem/author/Author.kt
@@ -1,9 +1,16 @@
 package com.example.bookmanagementsystem.author
 
+import com.example.bookmanagementsystem.ValidationException
 import com.fasterxml.uuid.Generators
 import java.time.LocalDate
 
-data class Author(val id: String, val name: String, val birthday: LocalDate?) {
+class Author(val id: String, val name: String, val birthday: LocalDate?) {
+    init {
+        val today = LocalDate.now()
+        if (birthday != null && !birthday.isBefore(today))
+            throw ValidationException("著者の誕生日が現在の日付以降になっています。id: $id, birthday: $birthday, today: $today")
+    }
+
     companion object {
         fun from(name: String, birthday: LocalDate?): Author {
             val id = Generators.timeBasedGenerator().generate().toString()
